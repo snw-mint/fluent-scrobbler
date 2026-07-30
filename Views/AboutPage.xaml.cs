5630cbfbf07d154f001339a139706f97c2bc48bb
Anndrey Abranches <snowmint.dev@gmail.com>
2026-07-30 19:01:46 -0300

    feat(info): add app and system info.

--- Views/AboutPage.xaml.cs
+++ Views/AboutPage.xaml.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Windows.ApplicationModel.DataTransfer;
 
 namespace FluentScrobbler.Views
 {
@@ -7,6 +12,31 @@ namespace FluentScrobbler.Views
         public AboutPage()
         {
             this.InitializeComponent();
+            this.Loaded += AboutPage_Loaded;
+        }
+
+        private void AboutPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            OsVersionText.Text = RuntimeInformation.OSDescription;
+            ArchitectureText.Text = $"{RuntimeInformation.OSArchitecture} (Process: {RuntimeInformation.ProcessArchitecture})";
+            RuntimeVersionText.Text = RuntimeInformation.FrameworkDescription;
+        }
+
+        private async void CopySystemInfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            string info = $"- App: Fluent Scrobbler v0.1\n" +
+                          $"- OS: {RuntimeInformation.OSDescription}\n" +
+                          $"- Architecture: {RuntimeInformation.OSArchitecture} (Process: {RuntimeInformation.ProcessArchitecture})\n" +
+                          $"- Framework: {RuntimeInformation.FrameworkDescription}\n" +
+                          $"- WinUI: Windows App SDK 1.5";
+
+            var dp = new DataPackage();
+            dp.SetText(info);
+            Clipboard.SetContent(dp);
+
+            CopyButtonText.Text = "Copied to Clipboard!";
+            await Task.Delay(2000);
+            CopyButtonText.Text = "Copy System Info for Bug Report";
         }
     }
 }
